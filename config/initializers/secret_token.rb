d237414cf924e00b28d1e83dacdc3c8c6034cd77
Jason Cluck <jcluck@ycp.edu>
2012-09-17 23:32:50 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BusTracker::Application.config.secret_token = '95c5e18f734e7d385ce92a5bf6d7f24541e4665d21a5b66357848364ff2bb0ed8c5b122a2618760f2dff0f18cec0204cac2ec013d5f59024bcef0609af848e5d'
