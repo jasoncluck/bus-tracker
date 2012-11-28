f41f976997d5628789f56c20790b4c1ead3a8cdd
Ian Will <ian.will@nrl.navy.mil>
2012-11-27 23:23:45 -0500

    Made bus valid its fields

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,5 +1,6 @@
 class Bus < ActiveRecord::Base
   attr_accessible :lat, :lon, :headsign, :wmataid, :busid, :direction, :dev, :last_update
+  validates :lat, :lon, :headsign, :wmataid, :busid, :direction, :dev, :last_update, :presence => true
   belongs_to :map
   has_one :route
 end
