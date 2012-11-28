47ffcc1d3064ae23bdb998ae395dab65c498c9a4
Ian Will <ian.will@nrl.navy.mil>
2012-11-27 23:23:22 -0500

    Added show_debug messages in poll buses to track status of AJAX requests

--- app/assets/javascripts/pollbuses.js
+++ app/assets/javascripts/pollbuses.js
@@ -19,22 +19,28 @@ if (!request)
  alert("Error initializing XMLHttpRequest!");
 
 function poll(){
+    show_debug('polling...');
     var url = "/buses.json"
     request.onreadystatechange = newpositions;
     request.open("GET", url, true);
     request.send(null);
+    show_debug('sent request..');
 }
 
 function newpositions()
 {
+    show_debug("received response, state "+request.readyState);
     if (request.readyState == 4) {
         if(request.status == 200){
             var positionsJSON = jQuery.parseJSON(request.responseText);
             //updateMarkeres is defined in mapbuses.js
+            show_debug("updating markers... ");
             updateMarkers(positionsJSON);
+            show_debug("(done)");
         }else{
             //alert("HTTP status: "+request.status);
         }
+        show_debug("resetting timeout... ");
         setTimeout(poll, 10000);
     }
 }
