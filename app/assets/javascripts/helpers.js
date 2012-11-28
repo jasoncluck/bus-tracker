02d6d9d1371fe9dc645afaa2090469372e50c2e6
Ian Will <ian.will@nrl.navy.mil>
2012-11-27 23:15:30 -0500

    Added show_debug helper function in javascript
     - It shows a string message on the top right corner, where the username
      normally is

--- app/assets/javascripts/helpers.js
+++ app/assets/javascripts/helpers.js
@@ -7,27 +7,16 @@ function get_random_color() {
     return color;
 }
 
+function show_debug(msg){
+  $("#navbar_right").text(msg);
+}
+
 //from http://stackoverflow.com/questions/1274743/how-can-i-determine-a-timezone-by-the-utc-offset/1274752#1274752
 //@param timestamp An ISO-8601 timestamp in the form YYYY-MM-DDTHH:MM:SS±HH:MM
 //Note: Some other valid ISO-8601 timestamps are not accepted by this function
 function parseISO8601(timestamp)
 {
-  var regex = new RegExp("^([\\d]{4})-([\\d]{2})-([\\d]{2})T([\\d]{2}):([\\d]{2}):([\\d]{2})Z");
-  var matches = regex.exec(timestamp);
-  if(matches != null)
-  {
-    return new Date(
-      Date.UTC(
-        parseInt(matches[1], 10),
-        parseInt(matches[2], 10) - 1,
-        parseInt(matches[3], 10),
-        parseInt(matches[4], 10),
-        parseInt(matches[5], 10),
-        parseInt(matches[6], 10)
-      )
-    );
-  }
-  return null;
+  return new Date(timestamp);//.strftime('%Y-%m-%dT%H:%M:%S%z', true);
 }
 
 //Returns true if the date is stale
