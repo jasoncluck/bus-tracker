c35b893417aec696e842918a49aa0c6c99283536
Ian Will <ian.will@nrl.navy.mil>
2012-11-28 23:40:11 -0500

    Implemented KML views of /buses and /buses/:id

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,6 +1,8 @@
+include KML
+
 class Route < ActiveRecord::Base
   attr_accessible :name, :direction, :routeid
   validates :name, :direction, :routeid, :presence => true
 
-  has_many :route_points
+  has_many :route_points, :dependent => :destroy
 end
