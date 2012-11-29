60a66362a606a795f2898d84bff3e0383618e7c5
Ian Will <ian.will@nrl.navy.mil>
2012-11-29 10:36:41 -0500

    Fixed syntax error in routes_helper

--- app/helpers/routes_helper.rb
+++ app/helpers/routes_helper.rb
@@ -2,6 +2,6 @@ module RoutesHelper
 
 	# Returns a color that maps to the route
 	def route_color(route)
-		return "%08x" % ((rand * 0xffffff) | 0x7f000000
+		return "%08x" % ((rand * 0xffffff) | 0x7f000000 )
 	end
 end
