480bb1ef911a594dff00a099ae0070acf6dfed4b
Ian Will <ian.will@nrl.navy.mil>
2012-11-28 21:36:56 -0500

    Added show methods for routes controller

--- app/controllers/route_points_controller.rb
+++ app/controllers/route_points_controller.rb
@@ -8,4 +8,13 @@ class RoutePointsController < ApplicationController
 			format.json { render json: @route_points }
 		end
 	end
+
+	def show
+		@route_point = RoutePoint.find(params[:id])
+
+		respond_to do |format|
+			format.html { render :show, layout: 'nonmap' }
+			format.json { render json: @route_point }
+		end
+	end
 end
