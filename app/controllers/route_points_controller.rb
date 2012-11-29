b36fed045b0f97bf5504aa378d9c73a58d9b1ae8
Ian Will <ian.will@nrl.navy.mil>
2012-11-28 21:22:39 -0500

    Made route points controller render a table of route points

--- app/controllers/route_points_controller.rb
+++ app/controllers/route_points_controller.rb
@@ -1,2 +1,11 @@
 class RoutePointsController < ApplicationController
+
+	def index
+		@route_points = RoutePoint.all
+
+		respond_to do |format|
+			format.html { render :index, layout: 'nonmap' }
+			format.json { render json: @route_points }
+		end
+	end
 end
