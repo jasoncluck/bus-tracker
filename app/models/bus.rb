16d8c6b78bce9f1f11ec3ed43a04f4b2e646c41d
Ian Will <icwill@gmail.com>
2012-11-29 22:26:18 -0500

    Fixed syntax problem in bus.rb

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,6 +1,6 @@
 class Bus < ActiveRecord::Base
   attr_accessible :lat, :lon, :headsign, :wmataid, :busid, :direction, :dev, :last_update, :draw
-  validates :lat, :lon, :headsign, :wmataid, :busid, :direction, :dev, :last_update, :draw :presence => true
+  validates :lat, :lon, :headsign, :wmataid, :busid, :direction, :dev, :last_update, :draw, :presence => true
   belongs_to :map
   has_one :route
 end
