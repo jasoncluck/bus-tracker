7910c7baa96332c7e2e9f9b410f950fdc79a47ad
Ian Will <icwill@gmail.com>
2012-12-01 20:31:48 -0500

    Added stale time to bus info

--- app/helpers/buses_helper.rb
+++ app/helpers/buses_helper.rb
@@ -1,2 +1,14 @@
 module BusesHelper
+
+	def stale_time(bus)
+		last=@bus.last_update
+		staleness = (Time.now() - last) #in seconds
+		hour = (staleness / 1.hour).to_i
+		minutes = ((staleness-hour.hours) / 1.minute).to_i
+		if hour > 0
+			return "#{hour}:#{minutes}"
+			
+		end
+		return "#{minutes}"
+	end
 end
