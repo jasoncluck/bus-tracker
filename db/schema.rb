93d6622a782e38f392ce3bdfd9ac6e9f3cdc5bba
Jason Cluck <jcluck@gwmail.gwu.edu>
2012-12-02 14:17:25 -0500

    revert jscript tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121202032227) do
+ActiveRecord::Schema.define(:version => 20121202143121) do
 
   create_table "buses", :force => true do |t|
     t.string   "name"
