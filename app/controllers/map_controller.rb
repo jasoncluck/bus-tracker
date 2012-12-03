56a6e8563243c13d3232ecc6acc811f1ea523ba3
Jason Cluck <jcluck@gwmail.gwu.edu>
2012-12-03 13:05:09 -0500

    remove bad code

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,8 +2,6 @@ class MapController < ApplicationController
 
   def index
   	#everytime the site index is accessed update the bus and stop table
-
-    system "rake buses &"
     @buses = Bus.all
     
     respond_to do |format|
