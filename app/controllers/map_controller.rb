0bab3cfe31d85b65c79a3636781bf3accaf7c036
Jason Cluck <jcluck@gwmail.gwu.edu>
2012-12-03 15:44:03 -0500

    daemon fix

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -3,7 +3,7 @@ class MapController < ApplicationController
   def index
   	#everytime the site index is accessed update the bus and stop table
     @buses = Bus.all
-    
+	   
     respond_to do |format|
       format.html { render :index, layout: 'maplayout' }
       format.json { render json: @buses }
