bf9cca04507df6ca9ec6a16b200155a768c4d465
Jason Cluck <jcluck@gwmail.gwu.edu>
2012-12-05 20:27:23 -0500

    update bus table when index loads

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,8 +1,13 @@
 class MapController < ApplicationController
 
+require "#{Rails.root}/lib/wmata_helper"
+include WmataHelper
+
+
   def index
   	#everytime the site index is accessed update the bus and stop table
     @buses = Bus.all
+    updateBusTable
 	   
     respond_to do |format|
       format.html { render :index, layout: 'maplayout' }
